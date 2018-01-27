963af3b09e1bd2f71715c5aa5530fb80b9364ba2
charityyoungblood <youngblood.charity@gmail.com>
2018-01-27 15:53:14 -0800

    created ShoeSuggestions class and UIViewController

---
+++ ShoeLove/Model/ShoeSuggestions.swift
@@ -0,0 +1,9 @@
+//
+//  ShoeSuggestions.swift
+//  ShoeLove
+//
+//  Created by Charity Youngblood  on 1/27/18.
+//  Copyright © 2018 Charity Youngblood . All rights reserved.
+//
+
+import Foundation
