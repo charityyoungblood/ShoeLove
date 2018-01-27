12be1ef21f55086a9974ac95f5fc7b9ed0edb470
charityyoungblood <youngblood.charity@gmail.com>
2018-01-26 16:43:00 -0800

    created PricePreference, ShoeTypes classes/files - added to Model folder

---
+++ ShoeLove/Model/PricePreference.swift
@@ -0,0 +1,9 @@
+//
+//  PricePreference.swift
+//  ShoeLove
+//
+//  Created by Charity Youngblood  on 1/26/18.
+//  Copyright © 2018 Charity Youngblood . All rights reserved.
+//
+
+import Foundation
