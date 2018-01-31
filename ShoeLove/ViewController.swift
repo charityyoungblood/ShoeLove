eabee302046d3c5324964311030284dba1f100a0
Charity <Youngblood.charity@gmail.com>
2018-01-31 15:18:00 -0800

    added notes/project to-do's, added constants

--- ShoeLove/ViewController.swift
+++ ShoeLove/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = 
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,4 +29,75 @@ class ViewController: UIViewController {
 
 
 }
+
+//TODO:Set up the location manager here.
+
+
+
+
+
+
+
+//MARK: - Networking
+/***************************************************************/
+
+//Write the getWeatherData method here:
+
+
+
+
+
+
+
+//MARK: - JSON Parsing
+/***************************************************************/
+
+
+//Write the updateWeatherData method here:
+
+
+
+
+
+//MARK: - UI Updates
+/***************************************************************/
+
+
+//Write the updateUIWithWeatherData method here:
+
+
+
+
+
+
+//MARK: - Location Manager Delegate Methods
+/***************************************************************/
+
+
+//Write the didUpdateLocations method here:
+
+
+
+//Write the didFailWithError method here:
+
+
+
+
+
+//MARK: - Change City Delegate methods
+/***************************************************************/
+
+
+//Write the userEnteredANewCityName Delegate method here:
+
+
+
+//Write the PrepareForSegue Method here
+
+
+
+
+
+}
+
 
