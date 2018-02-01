825b385ab9879a31a55d3bbdbcc3373c7853908c
Charity <Youngblood.charity@gmail.com>
2018-01-31 16:07:11 -0800

    created new location manager object; added location manager delegate; user permission for GPS

--- ShoeLove/ViewController.swift
+++ ShoeLove/ViewController.swift
@@ -9,34 +9,36 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+// by adding CLLocationManagerDelegate - our ViewController class is a subclass of the UIViewController, and it conforms to the rules of the Core location location manager delegate (CLLocationManagerDelegate)
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "a3d337183a430d921a50f64a3f37677a" // this is the same as the API key
-
+    
+// create a new LocationManager object - store in an instance variable
+    
+    let locationManager = CLLocationManager()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //TODO:Set up the location manager here.
+        
+        locationManager.delegate = self // delegate method is an included method from Apple - this allows us to access the GPS data from the iPhone
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters// the higher the accuracy, the longer it's going to take and the more battery your app will use
+        locationManager.requestWhenInUseAuthorization() = // this asks the user for their permission to give app their current location 
+        
     }
   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
-           
     } 
 
-
 }
-
-//TODO:Set up the location manager here.
-
-
-
-
-
-
 
 //MARK: - Networking
 /***************************************************************/
