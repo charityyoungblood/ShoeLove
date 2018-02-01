248d3177e52e72b7dcb2abe64fcee34495c4f859
Charity <Youngblood.charity@gmail.com>
2018-02-01 11:41:01 -0800

    added additional label to shoeSuggestions controller

--- ShoeLove/ViewController.swift
+++ ShoeLove/ViewController.swift
@@ -29,7 +29,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self // delegate method is an included method from Apple - this allows us to access the GPS data from the iPhone
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters// the higher the accuracy, the longer it's going to take and the more battery your app will use
         locationManager.requestWhenInUseAuthorization() // this asks the user for their permission to give app their current location
-        locationManager.startUpdatingLocation() // asynchronous method, meaning it works in the background 
+        locationManager.startUpdatingLocation() // asynchronous method, meaning it works in the background
+        // Once the startUpdatingLocation method retrieves the location information - it reports back to the ViewController class
+        // In order for us to receive the location information - we have to add the didUpdateLocations method below
         
     }
   
@@ -77,12 +79,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 /***************************************************************/
 
 
-//Write the didUpdateLocations method here:
+//Write the didUpdateLocations method here: - this method tells the delegate, the ViewController class, that the location data is available
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
 
 
-
-//Write the didFailWithError method here:
-
+//Write the didFailWithError method here: - this method tells the delegate, the ViewController class, that the location manager was unable to retrieve a location value - this may be because the user has their phone in airplane mode or the user doesn't have wi-fi access
+    // if there was an error - we want to print to the console, what the error was and then tell the user that there was an issue retrieving their location
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 
 
 
