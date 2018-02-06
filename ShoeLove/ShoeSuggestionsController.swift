e64c64a79b4c2581d75768f0a9c6e1e722012d25
charityyoungblood <youngblood.charity@gmail.com>
2018-02-05 17:30:48 -0800

    added request for location services to ViewController class

--- ShoeLove/ShoeSuggestionsController.swift
+++ ShoeLove/ShoeSuggestionsController.swift
@@ -13,83 +13,8 @@ import CoreLocation
 
 class ShoeSuggestionsController: UIViewController, CLLocationManagerDelegate {
     
-    let locationManager = CLLocationManager() // this represents creation of a new CLLocationManager() object with no parameters and storing it into
-    // the variable "locationManager"
-    
-    // TODO: Check to see if your app is authorized to use location services and request permission to access user location (display pop up window)
-    func isAppAuthorized(){
-        print(CLLocationManager.authorizationStatus())// what is return value for authorizationStatus() method?
-        
-    }
-    
-    // TODO: Determine the availability of location services - Not all Core Location services are available on every device, and services may be temporarily unavailable in certain circumstances
-        // ** check the availability of that service using the methods of your CLLocationManager() object (this is set to your locationManager variable)**
-    func locationServicesAvailability() {
-        if CLLocationManager.locationServicesEnabled() { // why can't I use the locationManager instance variable here?
-            switch CLLocationManager.authorizationStatus() {
-            case .notDetermined, .restricted, .denied:
-                print("No access")
-            case .authorizedAlways, .authorizedWhenInUse:
-                print("Access")
-            }
-        } else {
-            print("Location services are not enabled")
-        }
-    }
-  
-    // TODO: Determine user location
-    
-    //**IMPORTANT TODO: ASKING USER FOR LOCATION PERMISSION** It is recommended that you request only when-in-use authorization whenever possible. If your app requests and receives when-in-use authorization, you can make a separate request for always authorization later.
-    
-    // **IMPORTANT TODO: PICKING SERVICE TYPE TO FETCH USER LOCATION** - To help save power, disable location services (or switch to a lower-power alternative) when you do not need the location data offered by the service. For example, you might disable location services when your app is in the background and would not use that data otherwise.
-    
-    
-    // TODO: create a function that requests permission/asks user to access their location - with popup
-    
-    // TODO: disable location services when your app is in the background to save user battery
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        isAppAuthorized()
-        //locationManager.delegate = self
-        //locationManager.requestWhenInUseAuthorization()
-        // Do any additional setup after loading the view.
-    }
-    
-    func startReceivingSignificantLocationChanges() {
-        let authorizationStatus = CLLocationManager.authorizationStatus()
-        if authorizationStatus != .authorizedAlways {
-            // if user has not authorized access to location information - display a popup to the user that requests authorization with message
-            // "We need to access your location to show you the nearest stores"
-            return
-        }
-        
-        if !CLLocationManager.significantLocationChangeMonitoringAvailable() {
-            // The service is not available.
-            return
-        }
-        locationManager.delegate = self
-        locationManager.startMonitoringSignificantLocationChanges()
-    }
-    
-    // TODO: Once location is found - stop searching for location
-    
-    // TODO: Inform ShoeSuggestionsController class of user location
-    
-    // TODO: Store user location - probably in a variable
-    
-    // TODO: Access user location/connect user location to google maps
-    
-    // TODO: Evaluate user price preferences
-    
-    // TODO: Access stores via Google maps, based on user price preferences
-    
-    // TODO: Display stores on Google maps nearest to the user location
-    
-    // TODO: Implement the method(s) for handling any potential failures in addition to the methods for receiving location-related data.
-  
-
-
+   // if this page will show the location and GPS data, does it still need assigned as a delegate?
+    // or can I use the methods in ViewController in this class
     
     //MARK: - 
     /***************************************************************/
