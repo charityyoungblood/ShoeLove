a4d6ba3dd08a00a240aaf5afccd123fb72f3b00f
Charity <Youngblood.charity@gmail.com>
2018-02-07 12:51:29 -0800

    added initial location to test MapView

--- ShoeLove/ShoeSuggestionsController.swift
+++ ShoeLove/ShoeSuggestionsController.swift
@@ -132,10 +132,18 @@ class ShoeSuggestionsController: UIViewController, CLLocationManagerDelegate, MK
     
     // TODO: create method to show shoe store locations
     
-    let map = MKMapView()
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+   
     func showStoreLocations() {
-        
-        map.delegate = self
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        let regionRadius: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+
     
     }
     }
